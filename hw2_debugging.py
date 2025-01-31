8050b05417d435a7e123139fd46da5eb98c358ca
Meseker Worku <mworkuk@ncsu.edu>
2025-01-30 20:01:15 -0500

    We have debugged this during a zoom meeting

--- hw2_debugging.py
+++ hw2_debugging.py
@@ -1,7 +1,8 @@
 import rand
 
 def mergeSort(arr):
-    if (len(arr) == 1):
+    # Base case had to be fixed to <= 1
+    if (len(arr) <= 1):
         return arr
 
     half = len(arr)//2
@@ -14,21 +15,28 @@ def recombine(leftArr, rightArr):
     mergeArr = [None] * (len(leftArr) + len(rightArr))
     while leftIndex < len(leftArr) and rightIndex < len(rightArr):
         if leftArr[leftIndex] < rightArr[rightIndex]:
-            rightIndex += 1
+            # The right index was being updated here, switched to left index
             mergeArr[leftIndex + rightIndex] = leftArr[leftIndex]
-        else:
             leftIndex += 1
+        else:
+            # The left index was being updated here, switched to right index
             mergeArr[leftIndex + rightIndex] = rightArr[rightIndex]
+            rightIndex += 1
 
+    # The size of the merge array was being calculated incorrectly
+    size = leftIndex + rightIndex
     for i in range(rightIndex, len(rightArr)):
-        mergeArr[leftIndex + rightIndex] = rightArr[i]
+        mergeArr[size] = rightArr[i]
+        size += 1
     
     for i in range(leftIndex, len(leftArr)):
-        mergeArr[leftIndex + rightIndex] = leftArr[i]
+        mergeArr[size] = leftArr[i]
+        size += 1
 
     return mergeArr
 
 arr = rand.random_array([None] * 20)
+print(arr)
 arr_out = mergeSort(arr)
 
 print(arr_out)
