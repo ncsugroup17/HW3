f65733cb2a558d6e9c2ddb940f8b56ebb21621de
Meseker Worku <mworkuk@ncsu.edu>
2025-01-30 20:43:14 -0500

    Fixed the code and I'm commiting changes

--- rand.py
+++ rand.py
@@ -1,8 +1,14 @@
+"""
+Import for subprocess
+"""
 import subprocess
 
 def random_array(arr):
+    """
+    Shuffle the array with random numbers.
+    """
     shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
+    for i, _ in enumerate(arr):
+        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
